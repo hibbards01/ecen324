69fc49d0f5126f5a687e0b5ab5983b42a25fef43
Sam <hibbards01@gmail.com>
2015-04-21 10:45:48 -0600

    First commit.

---
+++ lab_1.c
@@ -0,0 +1,9 @@
+/***********************************************************************
+* Program:
+*    Lab C_lab, ECEN 324 Learning C Lab
+*    Brother Jones, ECEN 324
+* Author:
+*    Samuel Hibbard
+* Summary:
+*    Convert the .cpp program to .c
+************************************************************************/
