d23b21d71d3412aee2d787184dea8f8dcde95e0d
Sam <hibbards01@gmail.com>
2015-04-21 19:12:30 -0600

    Did printf with integer and float numbers.

--- lab_1.c
+++ lab_1.c
@@ -37,7 +37,54 @@ int main (void)
     printf("Twice %i is %i\n", number, number * 2);
 
     //
-    // Section is mainly about outputting an integer
+    // Output Integer
+    //
+
+    int anInt = 99;
+    float aFloat = 43.2;
+
+    printf("\n");
+
+    // Output the integer with the default formatting.
+    printf("Integer: %d ###\n", anInt);
+
+    // Output the integer in a fixed field width of 10.
+    printf("Integer: %10d ###\n", anInt);
+
+    // Output the integer in a fixed field width of 10
+    // and left justified in the field.
+    printf("Integer: %-10d ###\n", anInt);
+
+    //
+    // Output Float
+    //
+
+    printf("\n");
+
+    // Output the float with the default formatting.
+    printf("  Float: %.1f ###\n", aFloat);
+
+    // Output the float in a fixed field width of 10.
+    printf("  Float: %-10.1f ###\n", aFloat);
+
+    // Output the float right justified in a field width of 10 with
+    // a precision of 2 places after the decimal point for the float.
+    printf("  Float: %10.2f ###\n", aFloat);
+
+    // Output the float with 3 places of precision after the decimal point but
+    // not giving a field width.
+    printf("  Float: %0.3f ###\n", aFloat);
+
+    // Output the float with 3 places of precision after the decimal point with
+    // a field width of 10.
+    printf("  Float: %10.3f ###\n", aFloat);
+
+    // Output the float with 4 places of precision after the decimal point with
+    // a field width of 10 and left justify the number.
+    printf("  Float: %-10.4f ###\n", aFloat);
+
+    //
+    // Create some code to output a table
     //
 
 
